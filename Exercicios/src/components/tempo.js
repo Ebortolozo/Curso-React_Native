c98ddaa80f11f7b44fc83804baa521c052fbee91
Ewerton Bortolozo Nunes Júnior <ewertonbortolozo2009@gmail.com>
2021-12-03 15:41:49 -0300

    Snippet

---
+++ Exercicios/src/components/tempo.js
@@ -0,0 +1,10 @@
+import React from 'React'
+import { Text } from 'react-native'
+import Estilo from './estilo'
+
+export default props => {
+   return ( 
+        <Text style={Estilo.txtG}>
+            
+        </Text>
+)}
